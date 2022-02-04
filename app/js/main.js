cde44b10b4862997e183d1d456bc8df4752b8bcb
Ali G.Jahed <aligjahed@gmail.com>
2022-02-04 13:18:39 -0500

    header-final

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+// Test
+console.log("Js is working");
+
+const menu_icon = document.querySelector(".header__menu-icon");
+const menu = document.querySelector(".header__menu");
+const html = document.getElementsByTagName("html");
+
+menu_icon.addEventListener("click", function () {
+  if (menu_icon.classList.contains("menu-icon-close")) {
+    menu_icon.classList.remove("menu-icon-close");
+    menu_icon.classList.add("menu-icon-open");
+
+    menu.classList.add("menu-open");
+    menu.classList.remove("menu-close");
+
+    html.style.overflow = "hidden";
+  } else {
+    menu_icon.classList.add("menu-icon-close");
+    menu_icon.classList.remove("menu-icon-open");
+
+    menu.classList.remove("menu-open");
+    menu.classList.add("menu-close");
+
+    html.style.overflow = "";
+  }
+});
